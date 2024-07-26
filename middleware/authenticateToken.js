17a59f78b7f5d6ba083aa04bccf700a77de1da1b
abdisetiakawan <bedikadiryt@gmail.com>
2024-07-26 23:54:26 +0700

    update

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,17 +1,32 @@
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+// Mengimpor modul jsonwebtoken untuk menangani JWT
+const jwt = require("jsonwebtoken");
 
+// Memuat variabel lingkungan dari file .env
+require("dotenv").config();
+
+// Middleware untuk mengautentikasi token JWT
 function authenticateToken(req, res, next) {
-  const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1];
+  // Mendapatkan header Authorization dari permintaan
+  const authHeader = req.headers["authorization"];
 
+  // Memisahkan token dari header jika ada
+  const token = authHeader && authHeader.split(" ")[1];
+
+  // Jika token tidak ada, kembalikan status 401 (Unauthorized)
   if (token == null) return res.sendStatus(401);
 
+  // Verifikasi token menggunakan rahasia yang disimpan di variabel lingkungan
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    // Jika terjadi kesalahan saat verifikasi, kembalikan status 403 (Forbidden)
     if (err) return res.sendStatus(403);
+
+    // Jika token valid, simpan informasi pengguna dalam objek permintaan
     req.user = user;
+
+    // Lanjutkan ke middleware atau route handler berikutnya
     next();
   });
 }
 
+// Mengekspor fungsi authenticateToken untuk digunakan di bagian lain aplikasi
 module.exports = authenticateToken;
--- middleware/authorizeAdmin.js
+++ middleware/authorizeAdmin.js
@@ -1,9 +1,13 @@
+// Middleware untuk mengotorisasi pengguna dengan peran admin
 function authorizeAdmin(req, res, next) {
-    if (req.user.role !== 'admin') {
-      return res.status(403).json({ message: 'Access denied' });
-    }
-    next();
+  // Memeriksa apakah peran pengguna adalah 'admin'
+  if (req.user.role !== "admin") {
+    // Jika bukan admin, kembalikan status 403 (Forbidden) dengan pesan 'Access denied'
+    return res.status(403).json({ message: "Access denied" });
   }
-  
-  module.exports = authorizeAdmin;
-  
+  // Jika pengguna adalah admin, lanjutkan ke middleware atau route handler berikutnya
+  next();
+}
+
+// Mengekspor fungsi authorizeAdmin untuk digunakan di bagian lain aplikasi
+module.exports = authorizeAdmin;
