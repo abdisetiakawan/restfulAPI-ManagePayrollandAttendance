586fcb4c327a78434919b74ecd0d90b1fc5009cd
abdisetiakawan <bedikadiryt@gmail.com>
2024-08-06 16:55:03 +0700

    update respond

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.send('Welcome to the Payroll and Attendance Management System');
+router.get("/", function (req, res, next) {
+  res.send(
+    "Welcome to the Payroll and Attendance Management System - Abdi Setiawan Testing cabang baru"
+  );
 });
 
 module.exports = router;
